a56026455e69a1b68fdca8ab3fdbaf13f6725aca
Davidhak <Davidhak@hotmail.com>
2019-12-03 11:30:15 +0100

    Såhär är bättre

--- src/Main/Math.java
+++ src/Main/Math.java
@@ -6,8 +6,9 @@ public class Math {
         int sum = first + second;
         return sum;
     }
-    public int sub(int first, int second){
-        int sum = first - second;
-        return sum;
+    public int sub(int Adam, int David){
+        int Bättre så = David - Adam;
+        return Bättre
+                // Bättre om vi inte är tillsammans
     }
 }
